5e7f776a2facddf044d1989a32d52b17095773d4
Kundan Kumar <rockingkundan26@gmail.com>
2020-09-18 19:19:18 +0530

    First Commit

---
+++ app/src/main/java/com/example/android/quakereport/Earthquake.java
@@ -0,0 +1,46 @@
+package com.example.android.quakereport;
+
+public class Earthquake {
+    /** to store the mMagnitude */
+    private Double mMagnitude;
+    /** mLocation to show the place where earthquake occured */
+    private String mLocation;
+    /** mDate to show the date of occurence */
+    private long mTimeInMilliseconds;
+    /** Website URL of the earthquake */
+    private String mUrl;
+
+    /**
+     * Constructs a new {@link Earthquake} object.
+     *
+     * @param magnitude is the magnitude (size) of the earthquake
+     * @param location is the city location of the earthquake
+     * @param timeInMilliseconds is the time in milliseconds (from the Epoch) when the
+     *  earthquake happened
+     */
+    public Earthquake(Double magnitude, String location, long timeInMilliseconds, String url) {
+        mMagnitude = magnitude;
+        mLocation = location;
+        mTimeInMilliseconds = timeInMilliseconds;
+        mUrl = url;
+    }
+
+    //Getter method for Url
+    public String getUrl() { return mUrl; }
+
+    //Getter method for Magnitude
+    public Double getMagnitude() {
+        return mMagnitude;
+    }
+
+    //Getter method for Place
+    public String getLocation() {
+        return mLocation;
+    }
+
+    //Getter method for Date
+    public long getTimeInMilliseconds() {
+        return mTimeInMilliseconds;
+    }
+
+}
---
+++ app/src/main/java/com/example/android/quakereport/EarthquakeAdapter.java
@@ -0,0 +1,191 @@
+package com.example.android.quakereport;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+import android.graphics.drawable.GradientDrawable;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
+
+import java.text.DecimalFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+
+public class EarthquakeAdapter extends ArrayAdapter<Earthquake> {
+
+    /**
+     * Declared a seprator for primary location and location offset
+     */
+    private static final String LOCATION_SEPARATOR = " of ";
+
+    //Declared primary location
+    private String primaryLocation;
+
+    //Declared offset location
+    private String offsetLocation;
+
+    /**
+     * This is our own custom constructor (it doesn't mirror a superclass constructor).
+     * The context is used to inflate the layout file, and the list is the data we want
+     * to populate into the lists.
+     *
+     * @param context        The current context. Used to inflate the layout file.
+     * @param earthquake     A List of Earthquake objects to display in a list
+     */
+    public EarthquakeAdapter(@NonNull Context context, @NonNull ArrayList<Earthquake> earthquake) {
+        super(context, 0, earthquake);
+    }
+
+    /**
+     * Provides a view for an AdapterView (ListView, GridView, etc.)
+     *
+     * @param position The position in the list of data that should be displayed in the
+     *                 list item view.
+     * @param convertView The recycled view to populate.
+     * @param parent The parent ViewGroup that is used for inflation.
+     * @return The View for the position in the AdapterView.
+     */
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+
+        // Check if the existing view is being reused, otherwise inflate the view
+        View listItemView = convertView;
+        if(listItemView == null) {
+            listItemView = LayoutInflater.from(getContext()).inflate(
+                    R.layout.earthquake_list_item, parent, false);
+        }
+
+        // Get the {@link AndroidFlavor} object located at this position in the list
+        Earthquake earthquake = getItem(position);
+        assert earthquake != null;
+
+        // Find the TextView in the earthquake_activity.xml layout with the ID
+        TextView mangnitudeTextview = listItemView.findViewById(R.id.magnitude_textview);
+        // Get the magnitude value from the current earthquake object and
+        // Format the magnitude to show 1 decimal place
+        String formattedMagnitude = formatMagnitude(earthquake.getMagnitude());
+        // Display the magnitude of the current earthquake in that TextView
+        mangnitudeTextview.setText(formattedMagnitude);
+
+        // Set the proper background color on the magnitude circle.
+        // Fetch the background from the TextView, which is a GradientDrawable.
+        GradientDrawable magnitudeCircle = (GradientDrawable) mangnitudeTextview.getBackground();
+
+        // Get the appropriate background color based on the current earthquake magnitude
+        int magnitudeColor = getMagnitudeColor(earthquake.getMagnitude());
+
+        // Set the color on the magnitude circle
+        magnitudeCircle.setColor(magnitudeColor);
+
+        // Get the place name from the current earthquake object and separate it in two parts
+        String originalLocation = earthquake.getLocation();
+
+        //If there is location separator and if not
+        if (originalLocation.contains(LOCATION_SEPARATOR)) {
+            String[] parts = originalLocation.split(LOCATION_SEPARATOR);
+            offsetLocation = parts[0] + LOCATION_SEPARATOR;
+            primaryLocation = parts[1];
+        } else {
+            offsetLocation = getContext().getString(R.string.near_the);
+            primaryLocation = originalLocation;
+        }
+
+        // Find the TextView in the earthquake_activity.xml layout with the ID version_name
+        TextView primaryLocationTextview = listItemView.findViewById(R.id.primary_location);
+        // Get the place name for the primary location of earthquake object
+        primaryLocationTextview.setText(primaryLocation);
+
+        // Find the TextView in the earthquake_activity.xml layout with the ID version_name
+        TextView locationOffsetTextview = listItemView.findViewById(R.id.offset_location);
+        // Get the place name for offset location of earthquake object
+        locationOffsetTextview.setText(offsetLocation);
+
+
+
+        // Create a new Date object from the time in milliseconds of the earthquake
+        Date dateObject = new Date(earthquake.getTimeInMilliseconds());
+
+        // Find the TextView with view ID date
+        TextView dateView = listItemView.findViewById(R.id.date);
+        // Format the date string (i.e. "Mar 3, 1984")
+        String formattedDate = formatDate(dateObject);
+        // Display the date of the current earthquake in that TextView
+        dateView.setText(formattedDate);
+
+        // Find the TextView with view ID time
+        TextView timeView = listItemView.findViewById(R.id.time);
+        // Format the time string (i.e. "4:30PM")
+        String formattedTime = formatTime(dateObject);
+        // Display the time of the current earthquake in that TextView
+        timeView.setText(formattedTime);
+
+
+        return listItemView;
+    }
+
+    private int getMagnitudeColor(Double magnitude) {
+        int magnitudeColorResourceId;
+        int magnitudeFloor = (int) Math.floor(magnitude);
+        switch (magnitudeFloor) {
+            case 0:
+            case 1:
+                magnitudeColorResourceId = R.color.magnitude1;
+                break;
+            case 2:
+                magnitudeColorResourceId = R.color.magnitude2;
+                break;
+            case 3:
+                magnitudeColorResourceId = R.color.magnitude3;
+                break;
+            case 4:
+                magnitudeColorResourceId = R.color.magnitude4;
+                break;
+            case 5:
+                magnitudeColorResourceId = R.color.magnitude5;
+                break;
+            case 6:
+                magnitudeColorResourceId = R.color.magnitude6;
+                break;
+            case 7:
+                magnitudeColorResourceId = R.color.magnitude7;
+                break;
+            case 8:
+                magnitudeColorResourceId = R.color.magnitude8;
+                break;
+            case 9:
+                magnitudeColorResourceId = R.color.magnitude9;
+                break;
+            default:
+                magnitudeColorResourceId = R.color.magnitude10plus;
+                break;
+        }
+        return ContextCompat.getColor(getContext(), magnitudeColorResourceId);
+    }
+
+    private String formatMagnitude(Double magnitude) {
+        DecimalFormat magnitudeFormat = new DecimalFormat("0.0");
+        return magnitudeFormat.format(magnitude);
+    }
+
+    /**
+     * Return the formatted date string (i.e. "Mar 3, 1984") from a Date object.
+     */
+    private String formatDate(Date dateObject) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("LLL dd, yyyy");
+        return dateFormat.format(dateObject);
+    }
+
+    /**
+     * Return the formatted date string (i.e. "4:30 PM") from a Date object.
+     */
+    private String formatTime(Date dateObject) {
+        SimpleDateFormat timeFormat = new SimpleDateFormat("h:mm a");
+        return timeFormat.format(dateObject);
+    }
+}
